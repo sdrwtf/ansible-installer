64dbf4030f4a5e468e87d5d9a53821f8f6ddea1c
Patrick Euerle <p.euerle@shutdown-r.wtf>
2021-10-13 13:31:16 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ansible-installer
-Bash script to install ansible on Ubuntu 20.04
+
+## About
+
+I wanted to have a simple bash script to install Ansible on Ubuntu 20.04, so here it is. The only thing it does is following the official [install instructions provided by Ansible](https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-ubuntu).
+
+> :warning: This script is only tested on Ubuntu 20.04! It might work on other debian based distros, try at own risk.
+
+## Usage
+
+1. Clone or download this repository to your local machine
+2. Make the script executeable
+   ```bash
+   chmod +x ansible-install.sh
+   ```
+3. Run script as root
+   ```bash
+   sudo ./ansible-install.sh
+   ```
+
+## Contributing
+
+Any contributions you make are greatly appreciated. If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue if you like.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+## Acknowledgments
+
+- [Dave James Miller's Bash General-Purpose Yes/No Prompt Function ("ask")](https://gist.github.com/davejamesmiller/1965569)
---
+++ ansible-install.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+#
+# Simple script to install ansible on Ubuntu 20.04
+# https://github.com/sdrwtf/ansible-installer
+#
+
+# Colors
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+YELLOW='\033[0;33m'       # Yellow
+BLUE='\033[0;34m'         # Blue
+NC='\033[0m'              # No Color
+
+# Start
+_main() {
+  _is_root
+  _os_version
+}
+
+# Check if script is run as root
+_is_root() {
+  if [[ "$EUID" -ne 0 ]]; then
+    _die "Script needs to be run as root\n"
+  fi
+}
+
+# Check OS and version
+_os_version() {
+  local os=""
+  local ver_id=""
+
+  if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+
+    local os=$NAME
+    local ver_id=$VERSION_ID
+  fi
+
+  if [[ ${os} != "Ubuntu" ]] || [[ ${ver_id} != "20.04" ]]; then
+
+    if [[ ${os} = "" ]] || [[ ${ver_id} = "" ]]; then
+      _warn "Could not detect OS. Please note\n" \
+            "        that this script is only tested for Ubuntu 20.04!\n"
+    else
+      _warn "Seems like you are running ${os} ${ver_id}. Please note\n" \
+            "        that this script is only tested for Ubuntu 20.04!\n"
+    fi
+
+
+    if _ask "Continue anyways?"; then
+      _install
+    else
+      _die "Aborted by user\n"
+    fi
+
+  else
+    _install
+  fi
+}
+
+# Main installation
+_install() {
+
+  _warn "Begin of installation. This consists of:\n" \
+        "        - update repositories\n" \
+        "        - install paket software-properties-common\n" \
+        "        - add PPA ansible/ansible\n" \
+        "        - install ansible\n"
+
+  if _ask "Continue?"; then
+
+    _info "Updating repositories OS\n"
+    apt update || \
+    _die "Could not update repositories\n"
+
+    _info "Installing paket software-properties-common\n"
+    apt install software-properties-common --yes || \
+    _die "Installation of paket software-properties-common failed\n"
+
+    _info "Adding PPA ansible/ansible\n"
+    add-apt-repository --yes --update ppa:ansible/ansible || \
+    _die "Could not add PPA ansible/ansible\n"
+
+    _info "Installing paket ansible\n"
+    apt install ansible --yes || \
+    _die "Could not install paket ansible\n"
+
+    _good "Done\n\n$(ansible --version)\n"
+  else
+    _die "Aborted by user\n"
+  fi
+}
+
+# Check OS and version
+_os_version() {
+  local os=""
+  local ver_id=""
+
+  if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+
+    local os=$NAME
+    local ver_id=$VERSION_ID
+  fi
+
+  if [[ ${os} != "Ubuntu" ]] || [[ ${ver_id} != "20.04" ]]; then
+
+    if [[ ${os} = "" ]] || [[ ${ver_id} = "" ]]; then
+      _warn "Could not detect OS. Please note\n" \
+            "        that this script is only tested for Ubuntu 20.04!\n"
+    else
+      _warn "Seems like you are running ${os} ${ver_id}. Please note\n" \
+            "        that this script is only tested for Ubuntu 20.04!\n"
+    fi
+
+
+    if _ask "Continue anyways?"; then
+      _install
+    else
+      _die "Aborted by user\n"
+    fi
+
+  else
+    _install
+  fi
+}
+
+# Great general-purpose function to ask Yes/No questions by Dave James Miller
+# https://gist.github.com/davejamesmiller/1965569
+_ask() {
+  local prompt default reply
+
+  if [[ ${2:-} = 'Y' ]]; then
+    prompt='Y/n'
+    default='Y'
+  elif [[ ${2:-} = 'N' ]]; then
+    prompt='y/N'
+    default='N'
+  else
+    prompt='y/n'
+    default=''
+  fi
+
+  while true; do
+
+    # Ask the question (not using "read -p" as it uses stderr not stdout)
+    echo -n "$1 [$prompt] "
+
+    # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+    read -r reply </dev/tty
+
+    # Default?
+    if [[ -z $reply ]]; then
+      reply=$default
+    fi
+
+    # Check if the reply is valid
+    case "$reply" in
+      Y*|y*) return 0 ;;
+      N*|n*) return 1 ;;
+    esac
+
+  done
+}
+
+_good() {
+  echo -e >&2 "\n${GREEN}:: INFO: $*${NC}"
+}
+
+_info() {
+  echo -e >&2 "\n${BLUE}:: INFO: $*${NC}"
+}
+
+_warn() {
+  echo -e >&2 "\n${YELLOW}:: WARN: $*${NC}"
+}
+
+_error() {
+  echo -e >&2 "\n${RED}:: ERROR: $*${NC}"
+}
+
+_die() {
+  echo -e >&2 "\n${RED}:: ABORT: $*${NC}"
+  exit 1
+}
+
+_main
